ed0cc723e2cb2303ee12f11992ee1b68dbc86a37
switcchy <103215851+switcchy@users.noreply.github.com>
2023-10-29 14:40:39 +0000

    new main version for chicken coup game (0.2).
    still figuring out the pyinstaller or whatever its called so you dont need python installed.

--- Cverkil.py
+++ Cverkil.py
@@ -44,7 +44,8 @@ splash_texts = [
     "The Isle of Cverkil awaits your chicken-hunting skills!",
     "Can you uncover the secrets of the rare chickens of Cverkil?",
     "Become the ultimate chicken hunter on this mystical island!",
-    "Dare to venture into the Isle of Cverkil and hunt for unique fowl.","The chickens are calling. Can you resist their clucks?",
+    "Dare to venture into the Isle of Cverkil and hunt for unique fowl.",
+    "The chickens are calling. Can you resist their clucks?",
     "Rumor has it that a legendary chicken roams these lands.",
     "The Isle of Cverkil is waiting for your poultry pursuit!",
     "Discover the wonders of rare poultry in your chicken-hunting adventure.",
@@ -54,8 +55,6 @@ splash_texts = [
     "Welcome, aspiring chicken whisperer, to the chicken hunter's paradise.",
     "Eggs-traordinary secrets are hidden in the feathers of rare chickens.",
     "The sky is the limit, especially when it's filled with flying chickens!",
-
-
 ]
 
 # Define the size of the map
@@ -78,12 +77,20 @@ chicken_inventory = []
 player_achievements = set()
 
 # Define NPC characters with side quests
+def create_npc(name, description, quest, locations):
+    return {
+        "name": name,
+        "description": description,
+        "quest": quest,
+        "location": random.choice(locations)
+    }
+
+npc_locations = [(8, 8), (10, 10), (5, 20)]  # Example locations for NPCs
 npcs = [
-    {
-        "name": "Farmer John",
-        "description": "A friendly farmer who has lost his prized chicken.",
-        "location": (8, 8),
-        "quest": {
+    create_npc(
+        "Farmer John",
+        "A friendly farmer who has lost his prized chicken.",
+        {
             "name": "Lost Chicken",
             "description": "Find Farmer John's lost chicken and return it to him.",
             "objectives": [
@@ -91,10 +98,44 @@ npcs = [
                 "Return the chicken to Farmer John.",
             ],
             "reward": "Gold coins",
-        }
-    },
-    # Define other NPCs and their quests here...
+        },
+        npc_locations
+    ),
+    create_npc(
+        "Hermit Hilda",
+        "A mysterious hermit who seeks an unusual egg for an ancient ritual.",
+        {
+            "name": "Hermit's Ritual",
+            "description": "Find a rare egg for Hermit Hilda's ancient ritual.",
+            "objectives": [
+                "Locate the Cave of Mysteries (D12).",
+                "Retrieve the unique egg from the cave.",
+                "Return the egg to Hermit Hilda.",
+            ],
+            "reward": "Mystical Knowledge",
+        },
+        npc_locations
+    ),
+    # Add more NPCs with quests and locations here...
 ]
+
+# Define islands with their features
+islands = {
+    "Island of Cverkil": {
+        "map_size": 30,  # Define the map size for Island of Cverkil
+        "player_position": (15, 15),
+        "chicken_locations": [],
+        "active_quests": [],
+        "dock_location": (7, 7),  # Define a dock location for Island of Cverkil
+    },
+    "Island of Enderclaw": {
+        "map_size": 40,
+        "player_position": (20, 20),
+        "chicken_locations": [],
+        "active_quests": [],
+    },
+    # Add more islands with their features here...
+}
 
 # Function to handle NPC interactions and quests
 def handle_npc_interaction(player_position):
@@ -107,7 +148,7 @@ def handle_npc_interaction(player_position):
             accept_quest = input("Accept the quest? (Y/N): ").upper()
 
             if accept_quest == "Y":
-                active_quests.append(npc["quest"])
+                islands[current_island]["active_quests"].append(npc["quest"])
                 print(f"Quest '{npc['quest']['name']}' added to your quest log.")
             else:
                 print(f"You declined {npc['name']}'s quest.")
@@ -116,7 +157,7 @@ def handle_npc_interaction(player_position):
 def quest_menu():
     while True:
         print("\nQuest Menu:")
-        for idx, quest in enumerate(active_quests):
+        for idx, quest in enumerate(islands[current_island]["active_quests"]):
             print(f"{idx + 1}. {quest['name']} - {quest['description']}")
 
         print("0. Back to the game")
@@ -125,20 +166,35 @@ def quest_menu():
             break
         try:
             quest_index = int(choice) - 1
-            if 0 <= quest_index < len(active_quests):
-                print(f"Quest: {active_quests[quest_index]['name']}")
-                print(f"Description: {active_quests[quest_index]['description']}")
+            if 0 <= quest_index < len(islands[current_island]["active_quests"]):
+                print(f"Quest: {islands[current_island]['active_quests'][quest_index]['name']}")
+                print(f"Description: {islands[current_island]['active_quests'][quest_index]['description']}")
                 print(f"Objectives:")
-                for obj in active_quests[quest_index]['objectives']:
+                for obj in islands[current_island]['active_quests'][quest_index]['objectives']:
                     print(f"  - {obj}")
-                print(f"Reward: {active_quests[quest_index]['reward']}")
+                print(f"Reward: {islands[current_island]['active_quests'][quest_index]['reward']}")
             else:
                 print("Invalid quest number.")
         except ValueError:
             print("Invalid input. Please enter a quest number.")
 
+# Function to display the map
+def display_map(player_position, chicken_locations, npcs):
+    for row in range(islands[current_island]["map_size"], 0, -1):
+        for col in range(1, islands[current_island]["map_size"] + 1):
+            if (col, row) == player_position:
+                print("P", end=" ")  # Player's position
+            elif (col, row) in [location for _, location in chicken_locations]:
+                print("C", end=" ")  # Chicken's position
+            elif (col, row) in [npc["location"] for npc in npcs]:
+                print("N", end=" ")  # NPC's position
+            else:
+                print(".", end=" ")  # Empty space
+        print()  # Move to the next row
+
 # Game loop
-active_quests = []  # Initialize active quests
+current_island = "Island of Cverkil"  # Set the initial island
+
 while True:
     # Randomly select a splash text
     splash_text = random.choice(splash_texts)
@@ -146,25 +202,13 @@ while True:
     # Display the welcome screen with the map
     print(splash_text)
     print("Can you find all the rare chickens on the island?")
-    print("Press 'E' to enter the game or 'Q' to quit.")
+    print(f"Press 'E' to enter the game on {current_island} or 'Q' to quit.")
 
-    # Generate initial chicken locations
-    chicken_locations = generate_chicken_locations()
+    # Generate initial chicken locations for the current island
+    islands[current_island]["chicken_locations"] = generate_chicken_locations()
 
     while True:
-        # Display the map
-        for row in range(map_size, 0, -1):
-            for col in range(1, map_size + 1):
-                if (col, row) == player_position:
-                    print("P", end=" ")  # Player's position
-                elif (col, row) in [location for _, location in chicken_locations]:
-                    print("C", end=" ")  # Chicken's position
-                for npc in npcs:
-                    if (col, row) == npc["location"]:
-                        print("N", end=" ")  # NPC's position
-                else:
-                    print(".", end=" ")  # Empty space
-            print()  # Move to the next row
+        display_map(islands[current_island]["player_position"], islands[current_island]["chicken_locations"], npcs)
 
         user_choice = input("Your choice: ").upper()
 
@@ -172,9 +216,9 @@ while True:
             print("You have chosen to exit the game. Goodbye!")
             break
         else:
-            # Game loop
+            # Game loop for the current island
             while True:
-                print(f"You are at position {player_position}.")
+                print(f"You are at position {islands[current_island]['player_position']}.")
                 action = input("Do you want to move (N/S/E/W), check inventory (I), check achievements (A), check quests (Q), or quit (X)? ").upper()
 
                 if action == "X":
@@ -182,18 +226,22 @@ while True:
                     break
                 elif action in ("N", "S", "E", "W"):
                     if action == "N":
-                        player_position = (player_position[0], player_position[1] + 1)
+                        islands[current_island]["player_position"] = (
+                            islands[current_island]["player_position"][0], islands[current_island]["player_position"][1] + 1)
                     elif action == "S":
-                        player_position = (player_position[0], player_position[1] - 1)
+                        islands[current_island]["player_position"] = (
+                            islands[current_island]["player_position"][0], islands[current_island]["player_position"][1] - 1)
                     elif action == "E":
-                        player_position = (player_position[0] + 1, player_position[1])
+                        islands[current_island]["player_position"] = (
+                            islands[current_island]["player_position"][0] + 1, islands[current_island]["player_position"][1])
                     elif action == "W":
-                        player_position = (player_position[0] - 1, player_position[1])
+                        islands[current_island]["player_position"] = (
+                            islands[current_island]["player_position"][0] - 1, islands[current_island]["player_position"][1])
 
-                    for chicken_type, location in chicken_locations:
-                        if location == player_position:
+                    for chicken_type, location in islands[current_island]["chicken_locations"]:
+                        if location == islands[current_island]["player_position"]:
                             print(f"You found a {chicken_type} chicken! ({chicken_types[chicken_type]})")
-                            chicken_locations.remove((chicken_type, location))
+                            islands[current_island]["chicken_locations"].remove((chicken_type, location))
                             chicken_inventory.append(chicken_type)
 
                             # Check and update achievements
@@ -226,9 +274,16 @@ while True:
                 if action == "Q":
                     quest_menu()  # Call the quest menu function
 
-                if not chicken_locations:
-                    print("Congratulations! You've found all the rare chickens on the Isle of Cverkil.")
-                    chicken_locations = generate_chicken_locations()
+                # Handle NPC interactions and quests for the current island
+                handle_npc_interaction(islands[current_island]["player_position"])
 
-                # Handle NPC interactions and quests
-                handle_npc_interaction(player_position)
+                if not islands[current_island]["chicken_locations"]:
+                    print(f"Congratulations! You've found all the rare chickens on the {current_island}.")
+                    islands[current_island]["chicken_locations"] = generate_chicken_locations()
+
+                # Check for map transition logic (e.g., move to another island)
+                def player_reaches_edge_of_map():
+                    x, y = islands[current_island]["player_position"]
+                    map_size = islands[current_island]["map_size"]
+
+                    return x <= 0 or y <= 0 or x > map_size or y > map_size
